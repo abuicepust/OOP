8bc4367ad5953af42b3931e4a8d144b39931a905
MD. ABU YOUSUF <sojibhp.180640@s.pust.ac.bd>
2023-05-22 23:51:47 +0600

    Add files via upload

---
+++ StringClass/main.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string str1 = "Abu";
+    string str2 = "Yousuf";
+    string str3;
+
+    str3 = str1 + str2;
+    int len1 = str1.size();
+    int len2 = str3.size();
+
+    cout<<len1<<endl;
+    cout<<str3<<endl;
+    cout<<len2<<endl;
+
+
+    return 0;
+}
